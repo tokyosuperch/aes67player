69b6e54bcf57975d14a7a62fa11459eae2961953
tokyosuperch <supachan@sfc.wide.ad.jp>
2019-10-15 12:09:15 +0900

    マルチキャスト受信に対応

--- udp_recv.c
+++ udp_recv.c
@@ -1,53 +1,186 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <stdlib.h>
 #include <string.h>
-#include <netinet/in.h>
- 
-int main(int argc, char** argv)
-{
-    int sd;
-    struct sockaddr_in addr;
- 
-    socklen_t sin_size;
-    struct sockaddr_in from_addr;
- 
-    char buf[2048]; // 受信バッファ
- 
-    // IPv4 UDP のソケットを作成
-    if((sd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket");
-        return -1;
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+/*
+ *  マルチキャスト情報
+ */
+struct multicast_info {
+    char *ipaddr;            /* マルチキャストのIPアドレス */
+    unsigned short port;     /* ポート番号 */
+
+    int sd;                  /* ソケットディスクリプタ */
+    struct sockaddr_in addr; /* マルチキャストのアドレス構造体 */
+};
+typedef struct multicast_info mc_info_t;
+
+#define MAXRECVSTRING 255  /* Longest string to receive */
+
+static int initialize(int argc, char *argv[], mc_info_t *info, char *errmsg);
+static int multicast_receiver(mc_info_t *info, char *errmsg);
+static int socket_initialize(mc_info_t *info, char *errmsg);
+static int multicast_receive(mc_info_t *info, char *errmsg);
+static void socket_finalize(mc_info_t *info);
+static int ptp_recv(char* msg);
+
+/*!
+ * @brief   main routine
+ * @return  成功ならば0、失敗ならば-1を返す。
+ */
+int main(int argc, char *argv[]) {
+    int rc = 0;
+    mc_info_t info = {0};
+    char errmsg[BUFSIZ];
+
+    rc = initialize(argc, argv, &info, errmsg);
+    if(rc != 0){ 
+        fprintf(stderr, "Error: %s\n", errmsg);
+        return(-1);
+    }   
+
+    rc = multicast_receiver(&info, errmsg);
+    if(rc != 0){ 
+        fprintf(stderr, "Error: %s\n", errmsg);
+        return(-1);
     }
- 
-    // 待ち受けるIPとポート番号を設定
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(22222);
-    addr.sin_addr.s_addr = INADDR_ANY; // すべてのアドレス宛のパケットを受信する
- 
-    // バインドする
-    if(bind(sd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        return -1;
+
+    return(0);
+}
+
+/*!
+ * @brief      初期化処理。
+ * @param[in]  argc   コマンドライン引数の数
+ * @param[in]  argv   コマンドライン引数
+ * @param[out] info   マルチキャスト情報
+ * @param[out] errmsg エラーメッセージ格納先
+ * @return     成功ならば0、失敗ならば-1を返す。
+ */
+static int initialize(int argc, char *argv[], mc_info_t *info, char *errmsg) {
+    /* if(argc != 3){
+        sprintf(errmsg, "usage: %s <multicast address> <port>", argv[0]);
+        return(-1);
+    } */
+
+    memset(info, 0, sizeof(mc_info_t));
+    info->ipaddr = "224.0.1.129";
+    info->port   = 319;
+
+    return(0);
+}
+
+/*!
+ * @brief      マルチキャストを受信する
+ * @param[in]  info   マルチキャスト情報
+ * @param[out] errmsg エラーメッセージ格納先
+ * @return     成功ならば0、失敗ならば-1を返す。
+ */
+static int multicast_receiver(mc_info_t *info, char *errmsg) {
+    int rc = 0;
+
+    /* ソケットの初期化 */
+    rc = socket_initialize(info, errmsg);
+    if(rc != 0) return(-1);
+
+    /* マルチキャストを送信する */
+    rc = multicast_receive(info, errmsg);
+
+    /* ソケットの終期化 */
+    socket_finalize(info);
+
+    return(rc);
+}
+
+/*!
+ * @brief      ソケットの初期化
+ * @param[in]  info   マルチキャスト情報
+ * @param[out] errmsg エラーメッセージ格納先
+ * @return     成功ならば0、失敗ならば-1を返す。
+ */
+static int socket_initialize(mc_info_t *info, char *errmsg) {
+    int rc = 0;
+    struct ip_mreq multicast_request;
+
+    /* ソケットの生成 : UDPを指定する */
+    info->sd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(info->sd < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
     }
- 
-    // 受信バッファの初期化
-    memset(buf, 0, sizeof(buf));
- 
-    // 受信 パケットが到着するまでブロック
-    // from_addr には、送信元アドレスが格納される
-    if(recvfrom(sd, buf, sizeof(buf), 0,
-                (struct sockaddr *)&from_addr, &sin_size) < 0) {
-        perror("recvfrom");
-        return -1;
+
+    /* アドレス構造体を作成する */
+    info->addr.sin_family = AF_INET;
+    info->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    info->addr.sin_port = htons(info->port);
+
+    /* ポートにバインドする*/
+    rc = bind(info->sd, (struct sockaddr *)&(info->addr),
+              sizeof(info->addr));
+    if(info->sd < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
     }
- 
-    // ソケットのクローズ
-    close(sd);
- 
-    // 受信データの出力
-    printf("%s\n", buf);
- 
-    return 0;
+
+    /* マルチキャストグループの設定を行う */
+    multicast_request.imr_multiaddr.s_addr = inet_addr(info->ipaddr);
+    multicast_request.imr_interface.s_addr = htonl(INADDR_ANY);
+    rc = setsockopt(info->sd, IPPROTO_IP, IP_ADD_MEMBERSHIP,
+                    (void *)&multicast_request, sizeof(multicast_request));
+    if(rc < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
+    }
+
+    return(0);
+}
+
+/*!
+ * @brief      マルチキャストを受信する
+ * @param[in]  info   マルチキャスト情報
+ * @param[out] errmsg エラーメッセージ格納先
+ * @return     成功ならば0、失敗ならば-1を返す。
+ */
+static int multicast_receive(mc_info_t *info, char *errmsg) {
+    char recv_msg[MAXRECVSTRING+1];
+    int recv_msglen = 0;
+
+    /* Receive a single datagram from the server */
+    recv_msglen = recvfrom(info->sd, recv_msg, MAXRECVSTRING, 0, NULL, 0);
+    if(recv_msglen < 0){
+        sprintf(errmsg, "(line:%d) %s", __LINE__, strerror(errno));
+        return(-1);
+    }
+
+    recv_msg[recv_msglen] = '\0';
+    // printf("Received: %s\n", recv_msg);    /* Print the received string */
+    ptp_recv(recv_msg);
+    return(0);
+}
+
+/*!
+ * @brief      ソケットの終期化
+ * @param[in]  info   マルチキャスト情報
+ * @return     成功ならば0、失敗ならば-1を返す。
+ */
+static void socket_finalize(mc_info_t *info) {
+    /* ソケット破棄 */
+    if(info->sd != 0) close(info->sd);
+
+    return;
+}
+
+static int ptp_recv(char* msg) {
+	// versionPTP この関数はPTPv1用
+	if (msg[0x00] == (char)0 && msg[0x01] == (char)1) {
+		printf("Received PTPv1 Message!\n");
+	} else {
+		printf("This is not a PTPv1 Message!\n");
+		return -1;
+	}
+	// subdomain _DFLT
+	printf("%c%c%c%c%c\n", msg[0x04], msg[0x05], msg[0x06], msg[0x07], msg[0x08]);
+	return 0;
 }
--- udp_send.c
+++ udp_send.c
@@ -26,12 +26,13 @@ int main(int argc, char** argv)
 	// 送信先アドレスとポート番号を設定する
 	// 受信プログラムと異なるあて先を設定しても UDP の場合はエラーにはならない
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(319);
+	addr.sin_port = htons(320);
 	addr.sin_addr.s_addr = inet_addr("224.0.1.129");
 	if (bind(sd, (struct sockaddr *)&addr, sizeof(addr)) < 0){
 		printf("%s\n", strerror(errno));
 	}
 
+	addr.sin_port = htons(319);
 	// パケットをUDPで送信
 	if(sendto(sd, ptpmsg(), 124, 0, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("sendto");
